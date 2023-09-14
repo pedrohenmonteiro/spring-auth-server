1aeb383bc0e1829ed5a839248fb3d1b462a95e8c
Pedro Monteiro <pedromont436@gmail.com>
2023-09-14 19:10:11 -0300

    implementing UserStoreConfig

---
+++ src/main/java/br/com/pedromonteiro/authserver/UserStoreConfig.java
@@ -0,0 +1,25 @@
+package br.com.pedromonteiro.authserver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class UserStoreConfig {
+    
+    @Bean
+    UserDetailsService userDetailsService() {
+        var userDetailsManager = new InMemoryUserDetailsManager();
+
+        userDetailsManager.createUser(
+            User.withUsername("user")
+            .password("{noop}password")
+            .roles("USER")
+            .build()
+        );
+
+        return userDetailsService();
+    }
+}
