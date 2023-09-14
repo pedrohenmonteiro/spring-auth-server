eddc4909c15f498540493f0a36fbe826f6defb70
Pedro Monteiro <pedromont436@gmail.com>
2023-09-14 18:40:56 -0300

    implementing securityFilterChain to authorize any authenticated req

--- src/main/java/br/com/pedromonteiro/authserver/SecurityFilterConfig.java
+++ src/main/java/br/com/pedromonteiro/authserver/SecurityFilterConfig.java
@@ -4,7 +4,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
@@ -16,6 +16,7 @@ import org.springframework.security.web.authentication.LoginUrlAuthenticationEnt
 public class SecurityFilterConfig {
     
  @Bean
+ @Order(1)
  SecurityFilterChain authServerSecurityFilterChain(HttpSecurity http) throws Exception {
     OAuth2AuthorizationServerConfiguration.applyDefaultSecurity(http);
 
@@ -26,5 +27,14 @@ public class SecurityFilterConfig {
         new LoginUrlAuthenticationEntryPoint("/login"))).oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
 
         return http.build();
+ }
+
+ @Bean
+ @Order(2)
+ SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+       http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+           .formLogin(withDefaults());     
+
+       return http.build();    
  }
 }
